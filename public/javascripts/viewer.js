b39e26f1152806900c073e180ab5b05d72620288
Ivo de Kler <ivo.de.kler@gmail.com>
2015-11-20 16:34:23 +0100

    added autosuggest

--- public/javascripts/viewer.js
+++ public/javascripts/viewer.js
@@ -10,7 +10,30 @@ eventHandlers['button#submit-search'] = { click: search };
 
 function searchKeyUp( e ) {
 	if( e.keyCode === 13 ) {
-		search();
+		return search();
+	}
+
+console.log( e.keyCode );
+	if( e.keyCode === 38 || e.keyCode === 40 ) return;
+
+	if( e.target.value.length > 1 ) {
+		ajaxRequest( apiUrl, { q: e.target.value }, function( data ) {
+			var dataList = document.querySelector( 'datalist#autosuggest' );
+			dataList.innerHTML = '';
+
+			if( data.features ) {
+				data.features.forEach( function( feature ) {
+					var firstPit = feature.properties.pits[ 0 ],
+							name = firstPit && firstPit.name,
+							option = document.createElement( 'option' );
+
+					if( name ) {
+						option.value = name;
+						dataList.appendChild( option );
+					}
+				} );
+			}
+		} );
 	}
 }
 
