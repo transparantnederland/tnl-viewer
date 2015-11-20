94d306f4042bb0bd5addebeed36296c8eebcd33e
Ivo de Kler <ivo.de.kler@gmail.com>
2015-11-20 16:16:13 +0100

    first commit. basic search

---
+++ public/javascripts/ajaxRequest.js
@@ -0,0 +1,36 @@
+function ajaxRequest(url, method, query, callback){
+	if(typeof method === 'object'){
+		callback = query;
+		query = method;
+		method = 'GET';
+	} else if(typeof method === 'function'){
+		callback = method;
+		query = {};
+		method = 'GET';
+	}
+
+	var queryKeys = Object.keys(query);
+	queryKeys.forEach(function(key, i){
+		url += (i ? '&' : '?') + key + '=' + query[key];
+	});
+
+	var httpRequest = new XMLHttpRequest()
+	httpRequest.onreadystatechange = function(e){
+		var response;
+		if(e.target.readyState === 4) {
+			response = e.target.response;
+
+			if( response[ 0 ] === '{' ) {
+				try{
+					response = JSON.parse( response );
+				} catch( e ) {
+					console.log('could not parse as JSON: ' + response);
+				}
+			}
+
+			callback(response);
+		}
+	};
+	httpRequest.open(method, url);
+	httpRequest.send();
+}
---
+++ public/javascripts/viewer.js
@@ -0,0 +1,108 @@
+var apiUrl = '//transparantnederland.nl:3001/search',
+		boundDelegates = {},
+    eventHandlers = {},
+		searchField;
+
+document.addEventListener( 'DOMContentLoaded', documentReady );
+
+eventHandlers[ 'input#search' ] = { keyup: searchKeyUp };
+eventHandlers['button#submit-search'] = { click: search };
+
+function searchKeyUp( e ) {
+	if( e.keyCode === 13 ) {
+		search();
+	}
+}
+
+function search(){
+	ajaxRequest( apiUrl, { q: document.querySelector( 'input#search' ).value }, function( data ) {
+		var container = document.querySelector( 'ul#search-results ');
+
+		if( data.features ) data.features.forEach( function( feature ) {
+			console.log( feature );
+			container.appendChild( createSearchResult( feature ) );
+		} );
+	} );
+}
+
+function createSearchResult( feature ) {
+	var element = document.createElement( 'li' ),
+			heading = document.createElement( 'h3' ),
+			anchor = document.createElement( 'a' ),
+			typeContainer = document.createElement( 'p' ),
+			typeLabel = document.createElement( 'label' ),
+			typeText = document.createTextNode( feature.properties.pits[ 0 ].type ),
+			sourceContainer = document.createElement( 'p' ),
+			sourceLabel = document.createElement( 'label' ),
+			sourceText = document.createTextNode( feature.properties.pits[ 0 ].dataset ),
+			dumpContainer = document.createElement( 'p' );
+
+	typeLabel.innerText = 'type:';
+	typeContainer.appendChild( typeLabel );
+	typeContainer.appendChild( typeText );
+	sourceLabel.innerText = 'bron:';
+	sourceContainer.appendChild( sourceLabel );
+	sourceContainer.appendChild( sourceText );
+
+	anchor.innerHTML = feature.properties.pits[0].name;
+	dumpContainer.innerHTML = JSON.stringify( feature, false, 2 );
+
+	heading.appendChild( anchor );
+	element.appendChild( heading );
+	element.appendChild( typeContainer );
+	element.appendChild( sourceContainer );
+	
+	//element.appendChild( dumpContainer );
+
+	return element;
+}
+
+function documentReady() {
+	console.log('document ready!');
+
+	initEventHandlers();
+
+	searchField = document.getElementById('search');
+}
+
+function initEventHandlers(){
+  Object.keys( eventHandlers ).forEach( bindHandlersForElement );
+
+  function bindHandlersForElement( nodeSelector ){
+    var handlers = eventHandlers[nodeSelector],
+        element = nodeSelector instanceof Element ? nodeSelector : document.querySelector( nodeSelector ) ;
+    if( element ) Object.keys( handlers ).forEach( bindEvent );
+    else Object.keys( handlers ).forEach( bindDelegate );
+
+    function bindEvent( eventName ){
+      element.addEventListener( eventName, handlers[eventName] );
+    }
+
+    function bindDelegate( eventName ){
+      if( !boundDelegates[eventName] ) {
+        boundDelegates[eventName] = {};
+        document.addEventListener( eventName, createDelegateHandler( eventName ) );
+      }
+      boundDelegates[ eventName ][ nodeSelector] = handlers[eventName];
+    }
+  }
+
+  function createDelegateHandler( eventName ) {
+    return function delegateEvent(e){
+      var delegates = boundDelegates[eventName],
+          target = e.target,
+          result = true, didAnyCancel;
+
+      while( target && result ){
+        didAnyCancel = Object.keys( delegates ).map( evaluateHandler );
+        result = !~didAnyCancel.indexOf(false);
+
+        target = target.parentNode;
+      }
+
+      function evaluateHandler( nodeSelector ){
+        if( target.matches && target.matches( nodeSelector ) ) return delegates[nodeSelector].call( target, e );
+      }
+    }
+  }
+}
