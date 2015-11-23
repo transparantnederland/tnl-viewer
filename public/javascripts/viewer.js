fa6ee010ed93cd71cc2af5c58621767aeed55642
Ivo de Kler <ivo.de.kler@gmail.com>
2015-11-23 17:18:42 +0100

    added search filtering

--- public/javascripts/viewer.js
+++ public/javascripts/viewer.js
@@ -7,6 +7,7 @@ document.addEventListener( 'DOMContentLoaded', documentReady );
 
 eventHandlers[ 'input#search' ] = { keyup: searchKeyUp };
 eventHandlers['button#submit-search'] = { click: search };
+eventHandlers[ 'input[type=checkbox].filter' ] = { change: toggleFilter };
 
 function searchKeyUp( e ) {
 	if( e.keyCode === 13 ) {
@@ -36,27 +37,152 @@ function searchKeyUp( e ) {
 	}
 }
 
+function toggleFilter( e ) {
+	var key = this.dataset.filterkey,
+			value = this.dataset.filtervalue,
+			state = this.checked;
+
+	filters[ key ][ value ].value = state;
+	
+	applyFilters();
+	updateFilters();
+	showFilters();
+	showSearchResults();
+}
+
+var filterableProperties = [
+			'type',
+			'dataset',
+			'name'
+		],
+		filters = {},
+		searchResults,
+		filteredResults;
+
 function search(){
 	ajaxRequest( apiUrl, { q: document.querySelector( 'input#search' ).value }, function( data ) {
-		var container = document.querySelector( 'ul#search-results ');
+		searchResults = data.features;
+		filteredResults = data.features.slice();
 
-		if( data.features ) data.features.forEach( function( feature ) {
-			container.appendChild( createSearchResult( feature ) );
+		updateFilters();
+		applyFilters();
+		showFilters();
+		showSearchResults();
+	} );
+}
+
+function updateFilters() {
+	filterableProperties.forEach( function( key ) {
+		var list = filters[ key ] = filters[ key ] || {};
+
+		Object.keys( list ).forEach( function( key ) {
+			list[ key ].count = 0; //reset count
+		} );
+
+		filteredResults.forEach( function( feature ) {
+			var pit0 = feature.properties.pits[0],
+					value = pit0[ key ],
+					item = list[ value ],
+					storedValue;
+
+			if( item ) {
+				storedValue = item.value;
+			} else item = list[ value ] = { count: 0 };
+
+			item.value = storedValue || false;
+			item.count++;
 		} );
 	} );
 }
 
-function createSearchResult( feature ) {
+function showFilters() {
+	var container = document.querySelector( 'ul#filtercontainer' );
+	container.innerText = '';
+	
+	Object.keys( filters ).forEach( function( key ) {
+		container.appendChild( createFilterGroup( key, filters[ key ] ) );
+	} );
+}
+
+function createFilterGroup( key, properties ) {
+	var	template = document.querySelector( '#filtergroup' ),
+			node = document.importNode( template.content, true ),
+			ul = node.querySelector( 'ul' );
+
+	node.querySelector( 'h3' ).textContent = key;
+
+	Object.keys( properties ).forEach( function( name ) {
+		var child = createFilterItem( name, properties[ name ] );
+		if( child ) ul.appendChild( child );
+
+		function createFilterItem( name, info ) {
+			if( !info.count ) return;
+
+			var template = document.querySelector( '#filteritem' ),
+					node = document.importNode( template.content, true ),
+					input = node.querySelector( 'input' );
+			
+			input.checked = info.value ? 'checked' : '';
+			input.dataset.filterkey = key;
+			input.dataset.filtervalue = name;
+
+			node.querySelector( '.name' ).textContent = name;
+			node.querySelector( '.count' ).textContent = info.count;
+
+			return node;
+		}
+	} );
+	
+	return node;
+}
+
+function applyFilters(){
+	var allowedPropertiesByKey = {};
+	
+	Object.keys( filters ).forEach( function( key ) {
+		var list = filters[ key ];
+
+		allowedPropertiesByKey[ key ] = [];
+
+		Object.keys( list ).forEach( function( property ) {
+			if( list[ property ].value ) allowedPropertiesByKey[ key ].push( property );
+		} );
+
+		if( !allowedPropertiesByKey[ key ].length ) delete allowedPropertiesByKey[ key ];
+	} );
+
+	filteredResults = searchResults.filter( function( feature ) {
+		var filtered = false;
+
+		Object.keys( allowedPropertiesByKey ).forEach( function( key ){
+			var list = allowedPropertiesByKey[ key ];
+			filtered = filtered || list.indexOf( feature.properties.pits[ 0 ][ key ] ) === -1;
+		} );
+
+		return !filtered;
+	} );
+}
+
+function showSearchResults(){
+	var container = document.querySelector( 'ul#search-results ');
+	container.innerText = '';
+
+	filteredResults.forEach( function( feature ) {
+		var pit0 = feature.properties.pits[0];
+		
+		container.appendChild( createSearchResult( pit0 ) );
+	} );
+}
+
+function createSearchResult( pit ) {
 	var template = document.querySelector( '#searchresult' ),
-			anchor = template.content.querySelector( 'h3 a' ),
-			typeText = template.content.querySelector( 'span.typetext' ),
-			sourceText = template.content.querySelector( 'span.sourcetext' );
+			node = document.importNode( template.content, true );
 
-	anchor.textContent = feature.properties.pits[0].name;
-	typeText.textContent = feature.properties.pits[ 0 ].type;
-	sourceText.textContent = feature.properties.pits[ 0 ].dataset;
+	node.querySelector( 'h3 a' ).textContent = pit.name;
+	node.querySelector( 'span.typetext' ).textContent = pit.type;
+	node.querySelector( 'span.sourcetext' ).textContent = pit.dataset;
 
-	return document.importNode( template.content, true );
+	return node;
 }
 
 function documentReady() {
