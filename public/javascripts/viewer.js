5171bb5ffe323766cc6cb088a2047ab287e2b2ca
Ivo de Kler <ivo.de.kler@gmail.com>
2015-11-24 14:17:01 +0100

    moved basic functionality to basic.js

--- public/javascripts/viewer.js
+++ public/javascripts/viewer.js
@@ -1,8 +1,4 @@
-var apiUrl = '//transparantnederland.nl:3001/search',
-		boundDelegates = {},
-    eventHandlers = {},
-    routeHandlers = {},
-		searchField;
+var apiUrl = '//transparantnederland.nl:3001/search';
 
 document.addEventListener( 'DOMContentLoaded', documentReady );
 
@@ -196,41 +192,7 @@ function createSearchResult( pit ) {
 	return node;
 }
 
-function makeSafe( uri ) {
-	return uri.replace( '/', '\\' );
-}
 
-function makeUri( string ) {
-	return string.replace( '\\', '/' );
-}
-
-function documentReady() {
-	initEventHandlers();
-
-	searchField = document.getElementById('search');
-
-	if( location.hash ) handleHash( location.hash.substring( 1 ) );
-}
-
-function hashChange( e ) {
-	var hash = location.hash.substring( 1 );
-	handleHash( hash );
-}
-
-function handleHash( hash ) {
-	if( !hash.length ) {
-		//reset view
-		document.querySelector( 'td.filtertd' ).innerText = '';
-		document.querySelector( 'td.search-results' ).innerText = '';
-		document.querySelector( '#pitcontainer' ).innerText = '';
-		document.querySelector( 'input#search' ).value = '';
-	} else {
-		var parts = hash.split('/');
-		if( routeHandlers[ parts[ 0 ] ] ) {
-			return routeHandlers[ parts.shift() ]( parts );
-		}
-	}
-}
 
 function pitHandler( routeParts ) {
 	// get back the original pit uri
@@ -270,46 +232,4 @@ function showPit( err, feature ) {
 	document.querySelector( 'td.search-results' ).innerText = '';
 
 	pitContainer.appendChild( node );
-}
-
-function initEventHandlers(){
-  Object.keys( eventHandlers ).forEach( bindHandlersForElement );
-
-  function bindHandlersForElement( nodeSelector ){
-    var handlers = eventHandlers[nodeSelector],
-        element = nodeSelector instanceof Element ? nodeSelector : document.querySelector( nodeSelector ) ;
-    if( element ) Object.keys( handlers ).forEach( bindEvent );
-    else Object.keys( handlers ).forEach( bindDelegate );
-
-    function bindEvent( eventName ){
-      element.addEventListener( eventName, handlers[eventName] );
-    }
-
-    function bindDelegate( eventName ){
-      if( !boundDelegates[eventName] ) {
-        boundDelegates[eventName] = {};
-        document.addEventListener( eventName, createDelegateHandler( eventName ) );
-      }
-      boundDelegates[ eventName ][ nodeSelector] = handlers[eventName];
-    }
-  }
-
-  function createDelegateHandler( eventName ) {
-    return function delegateEvent(e){
-      var delegates = boundDelegates[eventName],
-          target = e.target,
-          result = true, didAnyCancel;
-
-      while( target && result ){
-        didAnyCancel = Object.keys( delegates ).map( evaluateHandler );
-        result = !~didAnyCancel.indexOf(false);
-
-        target = target.parentNode;
-      }
-
-      function evaluateHandler( nodeSelector ){
-        if( target.matches && target.matches( nodeSelector ) ) return delegates[nodeSelector].call( target, e );
-      }
-    }
-  }
 }
